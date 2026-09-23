18c194de5eec77a250408eed639d5942a9130a77
7979hzd4gj-oss <7979hzd4gj@privaterelay.appleid.com>
2026-09-23 20:42:41 +0000

    Delete  close.js

--- commands/general/open.js / close.js
+++
@@ -1,8 +0,0 @@
-export default {
-  name: "close",
-  category: "ADMIN",
-  async execute(sock, m) {
-    await sock.groupSettingUpdate(m.key.remoteJid, 'announcement');
-    await sock.sendMessage(m.key.remoteJid, { text: "🔒 Groupe fermé!" }, { quoted: m });
-  }
-};
