787b32b8fefbf2fce5d55ebc2913a00078d3004a
7979hzd4gj-oss <7979hzd4gj@privaterelay.appleid.com>
2026-09-23 20:42:14 +0000

    Update  close.js

--- commands/general/open.js / close.js
+++ commands/general/open.js / close.js
@@ -1,9 +1,8 @@
 export default {
-  name: "open",
+  name: "close",
   category: "ADMIN",
   async execute(sock, m) {
-    const from = m.key.remoteJid;
-    await sock.groupSettingUpdate(from, 'not_announcement');
-    await sock.sendMessage(from, { text: "✅ Groupe ouvert!" }, { quoted: m });
+    await sock.groupSettingUpdate(m.key.remoteJid, 'announcement');
+    await sock.sendMessage(m.key.remoteJid, { text: "🔒 Groupe fermé!" }, { quoted: m });
   }
 };
