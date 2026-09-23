90ce013ad853e2632d369a8aca7d23ba591669db
7979hzd4gj-oss <7979hzd4gj@privaterelay.appleid.com>
2026-09-23 23:01:21 +0000

    Delete gjid.js

--- commands/general/info.js /id.js/gjid.js
+++
@@ -1,8 +0,0 @@
-export default {
-  name: "id",
-  alias: ["gjid"],
-  category: "GENERAL",
-  async execute(sock, m) {
-    await sock.sendMessage(m.key.remoteJid, { text: `🆔 *ID:* ${m.key.remoteJid}\n👤 *Ton ID:* ${m.key.participant || m.key.remoteJid}` }, { quoted: m });
-  }
-};
