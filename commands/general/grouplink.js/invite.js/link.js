14f85e4c28d90e07e95d96a9ba1313dea84054ff
7979hzd4gj-oss <7979hzd4gj@privaterelay.appleid.com>
2026-09-23 21:22:57 +0000

    Delete link.js

--- commands/general/grouplink.js/invite.js/link.js
+++
@@ -1,9 +0,0 @@
-export default {
-  name: "grouplink",
-  alias: ["invite","link"],
-  category: "ADMIN",
-  async execute(sock, m) {
-    const code = await sock.groupInviteCode(m.key.remoteJid);
-    await sock.sendMessage(m.key.remoteJid, { text: `🔗 https://chat.whatsapp.com/${code}` }, { quoted: m });
-  }
-};
