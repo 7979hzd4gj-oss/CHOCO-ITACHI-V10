d66c22df586b224eaa453b2078f89ef4c3a07ca1
7979hzd4gj-oss <7979hzd4gj@privaterelay.appleid.com>
2026-09-24 21:10:58 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,15 @@
-export default {
-  PREFIX: ".",
-  ownerName: "CHOCO",
+// CHOCO-ITACHI-V10 - CONFIG
+module.exports = {
   OWNER_NUMBER: "224611257942",
+  OWNER_NAME: "CHOCO-ITACHI",
+  BOT_NAME: "CHOCO-ITACHI-V10",
   BOT_PIC: "https://i.ibb.co/4ZzJ9x5t/itachi.jpg",
-  BOT_NAME: "CHOCO-ITACHI-V10"
-}
+  PREFIX: ".",
+  WARN_LIMIT: 3,
+  WELCOME: true,
+  GOODBYE: true,
+  ANTILINK: false,
+  VERSION: "10.0.0",
+  FOOTER: "© CHOCO-ITACHI-V10 🥷🍫",
+  SESSION_FOLDER: "./session"
+};
