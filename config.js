fd1f4f267273db94b1ab973e1f115be78aa9e68f
7979hzd4gj-oss <7979hzd4gj@privaterelay.appleid.com>
2026-09-24 13:28:49 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,7 @@
 export default {
-  botName: "CHOCO-ITACHI-V10",
-  ownerName: "CHOCO",
-  ownerNumber: "224611257942",
-  prefix: ".",
-  country: "🇬🇳",
-  version: "10.0.0",
-  ownerJid: "224611257942@s.whatsapp.net",
-  BOT_NAME: "CHOCO-ITACHI-V10",
-  OWNER_NUMBER: "224611257942",
   PREFIX: ".",
-  BOT_PIC: "https://files.catbox.moe/856dqf.jpeg",
-  OWNER_JID: "224611257942@s.whatsapp.net"
+  ownerName: "CHOCO",
+  OWNER_NUMBER: "224611257942",
+  BOT_PIC: "https://files.catbox.moe/4zq9yt.jpg",
+  BOT_NAME: "CHOCO-ITACHI-V10"
 }
