bef2c591b4564a803fe5371e52461d3fde4fc71f
7979hzd4gj-oss <7979hzd4gj@privaterelay.appleid.com>
2026-09-24 03:38:47 +0000

    Delete news.js

--- commands/general/news.js
+++
@@ -1,7 +0,0 @@
-export default {
-  name: "news",
-  category: "GENERAL",
-  async execute(sock, m) {
-    await sock.sendMessage(m.key.remoteJid, { text: "📰 *News du jour*\nFonction à brancher avec API news" }, { quoted: m });
-  }
-};
