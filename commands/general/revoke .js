e8af0325fc6e054b6ad275e5f0fc11a92fe0f796
7979hzd4gj-oss <7979hzd4gj@privaterelay.appleid.com>
2026-09-24 03:40:37 +0000

    Delete revoke .js

--- commands/general/revoke .js
+++
@@ -1,8 +0,0 @@
-export default {
-  name: "revoke",
-  category: "ADMIN",
-  async execute(sock, m) {
-    await sock.groupRevokeInvite(m.key.remoteJid);
-    await sock.sendMessage(m.key.remoteJid, { text: "✅ Lien révoqué!" }, { quoted: m });
-  }
-};
