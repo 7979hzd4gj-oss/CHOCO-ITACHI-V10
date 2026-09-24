53a14118c37aec39fb2ea3371d726fa0398d149a
7979hzd4gj-oss <7979hzd4gj@privaterelay.appleid.com>
2026-09-24 01:11:11 +0000

    Delete contact.js

--- commands/general/contact.js
+++
@@ -1,7 +0,0 @@
-export default {
-  name: "contact",
-  category: "GENERAL",
-  async execute(sock, m) {
-    await sock.sendMessage(m.key.remoteJid, { text: "📞 *Contact Dev:* Choco - wa.me/224000000000" }, { quoted: m });
-  }
-};
