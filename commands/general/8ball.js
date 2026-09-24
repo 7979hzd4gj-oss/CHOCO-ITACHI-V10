15c4e42008d5d75455e557e92e7df85443bbc030
7979hzd4gj-oss <7979hzd4gj@privaterelay.appleid.com>
2026-09-24 01:08:36 +0000

    Delete 8ball.js

--- commands/general/8ball.js
+++
@@ -1,9 +0,0 @@
-const rep = ["Oui chef!","Non!","Peut-être","Fonce!","Jamais!"];
-export default {
-  name: "8ball",
-  category: "GENERAL",
-  async execute(sock, m, args) {
-    if(!args[0]) return sock.sendMessage(m.key.remoteJid, { text: "❓ Pose une question!" }, { quoted: m });
-    await sock.sendMessage(m.key.remoteJid, { text: `🎱 ${rep[Math.floor(Math.random()*rep.length)]}` }, { quoted: m });
-  }
-};
